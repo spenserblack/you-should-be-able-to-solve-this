0a477452425cc22c28e66b8e2f3b33f8ad0ec70f
Spenser Black <spenserblack01@gmail.com>
2022-03-10 11:58:29 -0500

    Add testing and reorganize project

---
+++ javascript/bin.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const x = require('./lib/x');
+
+console.log('x - 7 === 19 + x:', x - 7 === 19 + x);
--- javascript/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const x = Infinity;
-
-exports.x = x;
-
-if (require.main === module) {
-  console.log('x - 7 === 19 + x:', x - 7 === 19 + x);
-}
