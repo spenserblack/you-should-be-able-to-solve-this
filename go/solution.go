633d64a1bdc5f53608e085f2b90e1149e1195117
Spenser Black <spenserblack01@gmail.com>
2022-04-13 16:31:29 -0400

    Add Go solution

---
+++ go/solution.go
@@ -0,0 +1,14 @@
+// Solves x - 7 = 19 + x
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// X is the value that matches x - 7 = 19 + x
+var x = math.Inf(1)
+
+func main() {
+	fmt.Printf("x - 7 = 19 + x: %v\n", x = 7 == 19 + x)
+}
